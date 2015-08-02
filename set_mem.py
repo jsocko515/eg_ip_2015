3a5b4cc4bc4ea567e43e544b773a4165a5442f99
Martin <martin.jones@ed.ac.uk>
2015-08-02 18:23:50 +0100

    init

--- set_mem.py
+++
@@ -1,7 +0,0 @@
-import psutil, os 
- 
-s = set(range(1000000))
-    
-process = psutil.Process(os.getpid()) 
-mem = process.get_memory_info().rss / 1000 
-print("Used this much memory: " + str(mem) + ' kb')
